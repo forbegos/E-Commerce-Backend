ce838e2709a80e686bc188e8421ee46cf89227d1
forbegos <forbegos@me.com>
2021-05-26 17:09:28 -0600

    update category route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const newData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!newData[0]) {
+      res.status(404).json({ message: "no category with that id!" });
+      return;
+    }
+    res.status(200).json(newData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
